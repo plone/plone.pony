5ef860bc7b588eaa7424c157a009aafd9188de88
Andreas Zeidler <az@zitc.de>
2008-10-08 16:13:24 +0000

    release the plone pony out in the wild

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from os.path import join
 
 name = 'plone.pony'
-version = '1.0'
+version = '1.0.1'
 readme = open("README.txt").read()
 history = open(join('docs', 'HISTORY.txt')).read()
 
